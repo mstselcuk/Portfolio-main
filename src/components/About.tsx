8f5d15f81f011273ae238fe8847e44b9bf8001e4
mstselcuk <mesut.selcuk45@gmail.com>
2025-10-06 16:37:43 +1100

    about me fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -29,6 +29,7 @@ export function About() {
             I’m a passionate Solar Design Engineer with a strong focus on delivering high-performance and sustainable energy solutions.
             Over the past few years, I’ve been involved in designing and optimizing solar systems across residential, commercial, and utility-scale projects.
             <br></br>
+            <br></br>
             My goal is to create efficient designs that not only meet technical standards but also make solar energy more accessible and reliable for everyone.
             Now based in Melbourne, I’m continuing to expand my expertise in renewable technologies and support the transition toward a cleaner, smarter energy future.
           </p>
